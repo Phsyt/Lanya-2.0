afae5ddd13d0e1ad71d0e20d7dd12ea145a75a0d
Phsyt <prohuzairstatusback@gmail.com>
2025-08-28 02:13:31 +0530

    Update and rename rob.js to Rob.js

--- commands/money/Rob.js
+++ commands/money/Rob.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { loadData, saveData } = require('../dataManager.js');
+const User = require('../../models/User');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,27 +8,26 @@ module.exports = {
   
   async execute(interaction) {
     const userId = interaction.user.id;
-    let userBalances = loadData();
+    let userData = await User.findOne({ userId });
     
-    // Initialize user data if it doesn't exist
-    if (!userBalances[userId]) {
-      userBalances[userId] = { balance: 0 };
+    if (!userData) {
+      userData = new User({ userId });
     }
 
     const robSuccess = Math.random() > 0.2; // 80% chance of success
-    const currentBalance = userBalances[userId].balance;
+    const currentBalance = userData.balance;
     let responseMessage;
 
     if (robSuccess) {
       const stolenAmount = Math.floor(Math.random() * (currentBalance + 1)) + 1;
-      userBalances[userId].balance += stolenAmount;
-      responseMessage = `💰 You successfully robbed the bank and got away with $${stolenAmount}! Your new balance is $${userBalances[userId].balance}.`;
+      userData.balance += stolenAmount;
+      responseMessage = `💰 You successfully robbed the bank and got away with $${stolenAmount}! Your new balance is $${userData.balance}.`;
     } else {
-      userBalances[userId].balance = 0;
+      userData.balance = 0;
       responseMessage = `🚨 You failed the robbery and were caught by the police! You lost all your money. Your balance is now $0.`;
     }
     
-    saveData(userBalances);
+    await userData.save();
     await interaction.reply(responseMessage);
   },
 };
