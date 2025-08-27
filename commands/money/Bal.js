c22c0cd12ae9fc165da789b7bc94a8413f78eb6a
Phsyt <prohuzairstatusback@gmail.com>
2025-08-28 02:10:11 +0530

    Update Bal.js

--- commands/money/Bal.js
+++ commands/money/Bal.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { loadData } = require('../dataManager.js');
+const User = require('../../models/User');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,10 +8,9 @@ module.exports = {
   
   async execute(interaction) {
     const userId = interaction.user.id;
-    const userBalances = loadData();
+    const userData = await User.findOne({ userId });
     
-    // Get the user's balance from the data object or default to 0
-    const balance = userBalances[userId]?.balance || 0;
+    const balance = userData?.balance || 0;
     
     await interaction.reply(`Your current balance is $${balance}.`);
   },
